b7954a4152bc5a4c461875ab3e4358f1f8f5c9b5
Kristiono Setyadi <viaris@yahoo.com>
2012-02-29 12:25:21 +0700

    Initial commit

---
+++ linksExtractor.py
@@ -0,0 +1,37 @@
+import sys
+
+def linksExtractor(text):
+	start = 0
+	status = 1
+	
+	while status != -1:
+		status = text.find("<a", start)
+		
+		if status != -1:
+			start = status
+			end = text.find("</a>", start)
+			hrefPos = text.find("href=", start)
+			linkPos = hrefPos + 6
+			linkEndPos = text.find("\"", linkPos)
+			linkStr = text[linkPos:linkEndPos]
+			descPos = text.find(">", start) + 1
+			descEndPos = end
+			descStr = text[descPos:descEndPos]
+			
+			if (linkStr.find("http") != -1):
+				print descStr
+				print linkStr + "\n"
+			
+			start = end
+
+
+try:
+	fileSrc = open("A_re-introduction_to_JavaScript.html", "r")
+except IOError:
+	print >> sys.stderr, "File could not be opened"
+	sys.exit(1)
+	
+lines = fileSrc.readlines()
+
+for line in lines:
+	linksExtractor(line)
